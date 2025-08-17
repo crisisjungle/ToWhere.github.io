446ae7cf949335f5e5c577449b0a42912d1ad8b9
crisisjungle <1251532417@qq.com>
2025-08-17 21:46:22 +0800

    Update base path for ToWhere repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/travel-story-gift/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ToWhere/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
